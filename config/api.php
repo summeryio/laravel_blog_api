07c517672ba260bda62a170bddeb4242b4f99d03
summeryio <1005526074@qq.com>
2020-04-18 11:22:28 +0800

    添加跨域

--- config/api.php
+++ config/api.php
@@ -4,9 +4,9 @@ return [
     // 接口频率限制
     'rate_limits' => [
         // 访问频率限制，次数/分钟
-        'access' =>  env('RATE_LIMITS', '60,1'),
+        'access' =>  env('RATE_LIMITS', '6000,1'),
 
         // 登录相关，次数/分钟
-        'sign' =>  env('SIGN_RATE_LIMITS', '10,1'),
+        'sign' =>  env('SIGN_RATE_LIMITS', '6000,1'),
     ]
 ];
